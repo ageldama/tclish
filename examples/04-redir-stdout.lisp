7fbe92061d0c755f94b18a214b5cb58a0b268ac8
jonghyouk yun <ageldama@gmail.com>
2026-05-14 00:29:01 +0900

    [DONE] stdout/stderr redirection + app-main

--- examples/04-redir-stdout.lisp
+++ examples/04-redir-stdout.lisp
@@ -7,17 +7,8 @@
 
 
 (defun main-redir-stdout ()
-  (app-main ()
-            (let ((chan (make-instance '<redir-to-outstream-chan>
-                                       :lisp-stream *standard-output*
-                                       :name      "xxx-stdout")))
-              (unwind-protect
-                   (progn (regist chan
-                                  :tcl-interp-ptr    *tcl-interp*
-                                  :tcl-std-chan-type +tcl-stdout+)
-                          (do+chk (tcl-eval :error? nil)
-                                  *tcl-interp*
-                                  "puts {안녕하슈! HELLO?!}"))
-                ;; cleanup:
-                (when chan (dealloc chan))))))
+  (app-main (:stdout-stream *standard-output*)
+            (do+chk (tcl-eval :error? nil)
+                    *tcl-interp*
+                    "puts {안녕하슈! HELLO?!}")))
 
--- src/app-main.lisp
+++ src/app-main.lisp
@@ -27,33 +27,58 @@
         stderr-stream
         )
      &rest body)
+
   ;;
   (let ((%stdout-chan (gensym))
         (%stderr-chan (gensym)))
     `(progn
-       ,(when before-create-interp  before-create-interp)
-       ,(when tcl-init-subsystems?  `(tcl-init-subsystems))
+       ,@(when before-create-interp  (list before-create-interp))
+       ,@(when tcl-init-subsystems?  `((tcl-init-subsystems)))
        (let* ((*tcl-interp*     (tcl-create-interp))
               (*do+chk/error?*  ,do+chk/error?)
-              ;; TODO
-              )
+              ,@(when stdout-stream
+                  `((,%stdout-chan
+                     (make-instance
+                      'tclish/redir-to-outstream-chan:<redir-to-outstream-chan>
+                      :lisp-stream ,stdout-stream
+                      :name        "redir-stdout"))))
+              ,@(when stderr-stream
+                  `((,%stderr-chan
+                     (make-instance
+                      'tclish/redir-to-outstream-chan:<redir-to-outstream-chan>
+                      :lisp-stream ,stderr-stream
+                      :name        "redir-stderr")))))
+
          (unwind-protect
-              (progn ,(when before-init  before-init)
-                     ,(when zip-filename
-                        `(mnt-zipfs ,zip-filename
-                                    :mnt-pointer ,zipfs-mnt-point
-                                    :zip-passwd  ,zip-passwd
-                                    :tcl-library-path ,zipfs-tcl-library-path
-                                    :tk-library-path  ,zipfs-tk-library-path))
+              (progn ,@(when before-init  (list before-init))
+                     ,@(when zip-filename
+                         `((mnt-zipfs ,zip-filename
+                                      :mnt-pointer ,zipfs-mnt-point
+                                      :zip-passwd  ,zip-passwd
+                                      :tcl-library-path ,zipfs-tcl-library-path
+                                      :tk-library-path  ,zipfs-tk-library-path)))
                      (do+chk (tcl-init) *tcl-interp*)
-                     ,(when tk-init? `(do+chk (tk-init) *tcl-interp*))
-                     ,(when after-init after-init)
+                     ,@(when tk-init? `((do+chk (tk-init) *tcl-interp*)))
+                     ,@(when stdout-stream
+                         `((tclish/redir-to-outstream-chan:regist
+                            ,%stdout-chan :tcl-interp-ptr    *tcl-interp*
+                                          :tcl-std-chan-type +tcl-stdout+)))
+                     ,@(when stderr-stream
+                         `((tclish/redir-to-outstream-chan:regist
+                            ,%stderr-chan :tcl-interp-ptr    *tcl-interp*
+                                          :tcl-std-chan-type +tcl-stderr+)))
+                     ,@(when after-init (list after-init))
                      (progn ,@body)
-                     ,(when tk-main-loop? `(tk-main-loop)))
+                     ,@(when tk-main-loop? `((tk-main-loop))))
+
            ;; cleanup:
            (progn
-             ,(when before-deinit before-deinit)
-             ,(when zip-filename
-                `(umnt-zipfs :mnt-point ,zipfs-mnt-point))
+             ,@(when before-deinit (list before-deinit))
+             ,@(when zip-filename
+                `((umnt-zipfs :mnt-point ,zipfs-mnt-point)))
+             ,@(when stderr-stream `((tclish/redir-to-outstream-chan:dealloc
+                                      ,%stderr-chan)))
+             ,@(when stdout-stream `((tclish/redir-to-outstream-chan:dealloc
+                                      ,%stdout-chan)))
              (tcl-delete-interp *tcl-interp*)
-             ,(when after-deinit after-deinit)))))))
+             ,@(when after-deinit (list after-deinit))))))))
