d7ca1bc6bcaf851a7558625c038bbe88d74c84ce
jonghyouk yun <ageldama@gmail.com>
2026-05-19 13:36:02 +0900

    [WIP] docstrings

--- src/do+chk.lisp
+++ src/do+chk.lisp
@@ -14,6 +14,16 @@ lisp errors))")
         (error? '*do+chk/error?*)
         (include-error-info? t))
      &rest args)
+  "Runs Tcl C API function (`FN`) and Checks its result-code.
+
+- `ARGS` is a list of arguments to be applied on `FN`.
+
+- `:INTERP` is a Tcl interpreter runs Tcl C API function (`FN`).
+- `:TCL-OK` is used to check the result-code of `FN`, specifies code
+  for \"No Errors\". (usually `+TCL-OK+`)
+- `:ERROR?` asks raising Lisp error condition instead of just returning error message strings.
+- `:INCLUDE-ERROR-INFO?` asks that Lisp error condition or error message strings should include the contents of Tcl `errorInfo` variable.
+"
   (let ((%rc (gensym))
         (%err-msg (gensym)))
     `(let ((,%rc (funcall (fdefinition (quote ,fn-name)) ,@args)))
@@ -33,7 +43,9 @@ lisp errors))")
 
 
 (defmacro with-tcl-error/thrown (&rest body)
+  "In the `BODY`, `DO+CHK` raises Lisp errors."
   `(let ((*do+chk/error?* t)) ,@body))
 
 (defmacro with-tcl-error/result (&rest body)
+  "In the `BODY`, `DO+CHK` returns error message strings without raising Lisp errors."
   `(let ((*do+chk/error?* nil)) ,@body))
