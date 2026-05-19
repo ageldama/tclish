6e581e39e88d9822951774d03ef87901b7a4ac10
jonghyouk yun <ageldama@gmail.com>
2026-05-19 15:48:30 +0900

    [WIP] docstrings

--- src/error.lisp
+++ src/error.lisp
@@ -16,17 +16,21 @@
          condition args))
 
 
-(defun tcl-result-as-error
-    (&rest args)
+(defun tcl-result-as-error (&rest args)
+  "Extracts `Tcl_GetStringResult` and Converts it into a Lisp condition.
+
+If `*DO+CHK/ERROR?*`=`T`, also raises error condition."
   (let ((err-msg (tcl-get-string-result *tcl-interp*)))
     (apply #'pack-tcl-error :message err-msg args)))
 
 
-
 (defun set-tcl-result-string (s)
+  "Sets Tcl result state with Lisp string `S`."
   (cffi:with-foreign-string (str-ptr s)
     (tcl-set-obj-result *tcl-interp*
                         (tcl-new-string-obj str-ptr -1))))
 
 (defun set-tcl-result-from-error (an-error)
+  "Sets Tcl result state with a string message of Lisp condition
+`AN-ERROR`."
   (set-tcl-result-string (format nil "~a" an-error)))
