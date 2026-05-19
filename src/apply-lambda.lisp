b4df0d50e6fc47ccc6f2748551b141ccb888f152
jonghyouk yun <ageldama@gmail.com>
2026-05-19 12:52:22 +0900

    [WIP] docstrings

--- src/apply-lambda.lisp
+++ src/apply-lambda.lisp
@@ -3,6 +3,10 @@
 (defun apply-lambda
     (tcl-lambda args
      &key (result-as :string))
+  "Apply `ARGS` on Tcl lambda-list (`TCL-LAMBDA`): https://www.tcl-lang.org/man/tcl/TclCmd/apply.html
+
+`:RESULT` keyword parameter is `(MEMBER (:STRING :OBJ)` (See
+`RESULT-AS`)"
   (with-tcl-objv
       (`("apply" ,tcl-lambda ,@args))
       (progn
