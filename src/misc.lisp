eb8d8a952b59ee1a28164886d9317732508139ba
jonghyouk yun <ageldama@gmail.com>
2026-05-19 15:42:00 +0900

    [WIP] docstrings

--- src/misc.lisp
+++ src/misc.lisp
@@ -4,7 +4,11 @@
 
 
 (defun def-cmd/p (&key (cmd-name "p"))
+  "Defines `CMD-NAME`=`p` Tcl command.
+
+Which simply pretty-prints arguments as Tcl strings to
+`*STANDARD-OUTPUT*`."
   (def-cmd (cmd-name :interp *tcl-interp*)
-           (format t "~{~A~}~%" (cdr tclish:args))))
+           (format t "~{~W~}~%" (cdr tclish:args))))
 
 
